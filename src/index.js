2aec2ee87d51572bb194e5173780f3d19f5ed46c
Vitaly898 <vetalpetrov2016@yandex.ru>
2020-10-20 15:25:44 +0300

    init : finish task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+let arr10 = [];
+    for(let i=0;i<expr.length; i+=10){
+      arr10.push(expr.substr(i,10))
+    }
+
+let result = '';
+arr10.forEach(el=>{
+    if (el === '**********'){
+        result+=' ';
+    }
+    else
+    {
+        result+=MORSE_TABLE[toMorse(el)]
+    }
+})
+return result;
+
 }
 
 module.exports = {
     decode
 }
+
+
+let toMorse = function (expr10){
+let morseString= '';
+let arr = [];
+    for(let i=0;i<expr10.length; i+=2)
+    {
+      arr.push(expr10.substr(i,2))
+    }
+  for(j=0;j<arr.length;j++)
+{
+    if(arr[j] !==0)
+    {
+    if(arr[j]==='01' || arr[j]==='10'){
+    morseString+= '.'
+     }
+        if(arr[j]==='11'){
+        morseString+='-'
+        }
+    }
+}
+
+return morseString;
+}
+
+
